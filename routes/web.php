5d49b7769d2f277eff2565b728bd383cd62f4f0b
Nirdesh <nirdeshchauhan878@gmail.com>
2025-04-05 16:56:26 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ Route::get('/env-check', function () {
     return env('APP_NAME', 'Not loaded');
 });
 
+// routes/web.php
+Route::get('/health', function () {
+    return [
+        'app_name' => config('app.name'),
+        'env' => config('app.env'),
+        'db' => DB::connection()->getDatabaseName(),
+    ];
+});
+
