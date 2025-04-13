adc3817d2961e33c987b3fc0a3e05076426bdfff
Nirdesh <nirdeshchauhan878@gmail.com>
2025-04-13 16:49:33 +0530

    google login

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,16 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
+        'register' 
+    ],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://laravel-app-mjrd.onrender.com'], // Replace with your app's domain in production
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
